4a410c727677b81310c08f1db7594cdb269888f1
Liliana Franco <lilimafra@hotmail.com>
2023-05-31 22:11:38 -0500

    background

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
 
+var intervalId;
+
+function startColorChange() {
+  var startButton = document.querySelector('[data-start]');
+  var stopButton = document.querySelector('[data-stop]');
+  startButton.disabled = true;
+  stopButton.disabled = false;
+
+  intervalId = setInterval(function () {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopColorChange() {
+  var startButton = document.querySelector('[data-start]');
+  var stopButton = document.querySelector('[data-stop]');
+  startButton.disabled = false;
+  stopButton.disabled = true;
+
+  clearInterval(intervalId);
+}
+
+document
+  .querySelector('[data-start]')
+  .addEventListener('click', startColorChange);
+document
+  .querySelector('[data-stop]')
+  .addEventListener('click', stopColorChange);

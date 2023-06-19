55c55fcc9fdc05f26ff7e6b6272677d6f94237e2
Liliana Franco <lilimafra@hotmail.com>
2023-06-19 01:00:00 -0500

    Prueba

---
+++ src/js/02-timer.js
@@ -0,0 +1,78 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const datetimePicker = document.getElementById('datetime-picker');
+const startButton = document.querySelector('[data-start]');
+const daysElement = document.querySelector('[data-days]');
+const hoursElement = document.querySelector('[data-hours]');
+const minutesElement = document.querySelector('[data-minutes]');
+const secondsElement = document.querySelector('[data-seconds]');
+
+let countdownInterval;
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateTimerDisplay(time) {
+  daysElement.textContent = addLeadingZero(time.days);
+  hoursElement.textContent = addLeadingZero(time.hours);
+  minutesElement.textContent = addLeadingZero(time.minutes);
+  secondsElement.textContent = addLeadingZero(time.seconds);
+}
+
+function startCountdown(endDate) {
+  clearInterval(countdownInterval);
+
+  countdownInterval = setInterval(() => {
+    const currentTime = new Date().getTime();
+    const timeRemaining = endDate - currentTime;
+
+    if (timeRemaining <= 0) {
+      clearInterval(countdownInterval);
+      updateTimerDisplay({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      return;
+    }
+
+    const time = convertMs(timeRemaining);
+    updateTimerDisplay(time);
+  }, 1000);
+}
+
+flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+
+    if (selectedDate && selectedDate > new Date()) {
+      startButton.removeAttribute('disabled');
+    } else {
+      startButton.setAttribute('disabled', true);
+    }
+  },
+});
+
+startButton.addEventListener('click', () => {
+  const selectedDate = flatpickr.parseDate(datetimePicker.value);
+
+  if (selectedDate) {
+    startCountdown(selectedDate.getTime());
+  }
+});
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const delay = parseInt(form.delay.value);
+  const step = parseInt(form.step.value);
+  const amount = parseInt(form.amount.value);
+  let position = 1;
+
+  for (let i = 0; i < amount; i++) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    delay += step;
+    position++;
   }
 }
